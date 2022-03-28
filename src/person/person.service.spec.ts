a9a86a0f26db7140197e92ae9463bf2fd14c0061
David <david.krtolica8@gmail.com>
2022-03-28 12:54:47 +0200

    Seperated tests into 2 different test suites, dependent on being purely unit or integration

--- src/person/person.service.spec.ts
+++ src/person/person.service.spec.ts
@@ -4,31 +4,35 @@ import * as config from '../../ormconfig';
 import { PersonService } from './person.service';
 import { PostalCode } from './postal-code.entity';
 
-describe('Person service', () => {
-  let service: PersonService;
 
-  beforeAll(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      // declare the database connection
-      imports: [
-        TypeOrmModule.forRoot({
-          type: 'mysql',
-          host: config.default.host,
-          port: config.default.port,
-          username: config.default.user,
-          password: config.default.password,
-          database: config.default.schema,
-          entities: [PostalCode],
-          synchronize: true,
-          dropSchema: false,
-          keepConnectionAlive: true,
-        }),
-        TypeOrmModule.forFeature([PostalCode]),
-      ],
-      providers: [PersonService],
-    }).compile();
-    service = module.get<PersonService>(PersonService);
-  });
+let service: PersonService;
+
+beforeAll(async () => {
+  const module: TestingModule = await Test.createTestingModule({
+    // declare the database connection
+    imports: [
+      TypeOrmModule.forRoot({
+        type: 'mysql',
+        host: config.default.host,
+        port: config.default.port,
+        username: config.default.user,
+        password: config.default.password,
+        database: config.default.schema,
+        entities: [PostalCode],
+        synchronize: true,
+        dropSchema: false,
+        keepConnectionAlive: true,
+      }),
+      TypeOrmModule.forFeature([PostalCode]),
+    ],
+    providers: [PersonService],
+  }).compile();
+  service = module.get<PersonService>(PersonService);
+});
+
+
+//TEST SUITE FOR SIMPLE PERSON SERVICE UNIT TESTS
+describe('Person service unit tests', () => {
 
   //RANDOM STREET NAME TESTS
   it('should return a random street name that is not null', () => {
@@ -45,31 +49,6 @@ describe('Person service', () => {
     const randomStreetName = service.generateStreetName();
     expect(randomStreetName.length).toBeGreaterThanOrEqual(8);
     expect(randomStreetName.length).toBeLessThanOrEqual(25);
-  });
-
-  //RANDOM POSTAL CODE TESTS - DATABASE CONNECTION, INTEGRATION TESTS
-  it('should call findRandom method which returns a random post code which is not null', async () => {
-    const findData = await service.findRandom();
-    expect(findData).toBeDefined();
-  });
-
-  it('should call findRandom method which returns a random post code with according properties', async () => {
-    const findData = await service.findRandom();
-    expect(findData).toHaveProperty('code');
-    expect(findData).toHaveProperty('town');
-  });
-
-  it('should call findRandom method and check typeof properties', async () => {
-    const findData = await service.findRandom();
-    expect(typeof (findData.code)).toEqual('string');
-    expect(typeof (findData.town)).toEqual('string');
-  });
-
-  it('should call findRandom method and check if code is a 4-digit number', async () => {
-    const findData = await service.findRandom();
-    const codeNum = parseInt(findData.code);
-    expect(codeNum).toBeGreaterThanOrEqual(1000);
-    expect(codeNum).toBeLessThanOrEqual(9999);
   });
 
   //RANDOM ADDRESS NUMBERS TESTS
@@ -110,6 +89,38 @@ describe('Person service', () => {
   it('should generate a random door number of type string', () => {
     const randomDoorNum = service.generateDoorNumber();
     expect(typeof (randomDoorNum)).toEqual('string');
+  });
+
+});
+
+
+//TEST SUITE FOR PERSON SERVICE INTEGRATION TESTS FOR POSTAL CODE
+//ESTABLISHING A DATABASE CONNECTION
+describe('Person service integration tests for postal code', () => {
+
+  //RANDOM POSTAL CODE TESTS - DATABASE CONNECTION, INTEGRATION TESTS
+  it('should call findRandom method which returns a random post code which is not null', async () => {
+    const findData = await service.findRandom();
+    expect(findData).toBeDefined();
+  });
+
+  it('should call findRandom method which returns a random post code with according properties', async () => {
+    const findData = await service.findRandom();
+    expect(findData).toHaveProperty('code');
+    expect(findData).toHaveProperty('town');
+  });
+
+  it('should call findRandom method and check typeof properties', async () => {
+    const findData = await service.findRandom();
+    expect(typeof (findData.code)).toEqual('string');
+    expect(typeof (findData.town)).toEqual('string');
+  });
+
+  it('should call findRandom method and check if code is a 4-digit number', async () => {
+    const findData = await service.findRandom();
+    const codeNum = parseInt(findData.code);
+    expect(codeNum).toBeGreaterThanOrEqual(1000);
+    expect(codeNum).toBeLessThanOrEqual(9999);
   });
 
 });
